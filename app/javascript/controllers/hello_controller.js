f8d46526a2680cecc3a2317a821be96d62c5ac1f
Ubuntu <ubuntu@ip-172-31-40-153.ap-southeast-1.compute.internal>
2022-12-09 10:49:36 +0000

    Added Bootstrap Font Awesome cdn

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,12 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+import { Controller } from 'stimulus'; 
+export default class extends Controller {
+  connect() {
+    console.log("hello from StimulusJS")
+  }
+}
+welcome() {
+    console.log("click")
+  }
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'controllers'
+++ import * as bootstrap from 'bootstrap'
+++ import "../stylesheets/application"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
